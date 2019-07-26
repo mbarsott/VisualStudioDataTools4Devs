1897a02a42c68c230d69132f26685fcfbd4e63df
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-07-26 14:22:46 -0400

    4. Publishing Database Scripts

---
+++ AcmeCustomerManagement/ACM2.Database/dbo/Stored Procedures/CustomerRetrieve.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[CustomerRetrieve] 
+    @StartsWith AS VARCHAR(1)
+AS
+	SELECT 
+        Customer.CustomerId,
+        Customer.LastName,
+        Customer.FirstName,
+        EmailAddress,
+        CustomerTypeId,
+        PhoneNumber
+    FROM
+        Customer
+    WHERE
+        LastName LIKE @StartsWith + '%'
+    ORDER BY
+        LastName 
+RETURN 0
---
+++ AcmeCustomerManagement/ACM2.Database/dbo/Tables/CustomerType.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[CustomerType]
+(
+    [CustomerTypeId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Description] NVARCHAR(50) NULL
+)
